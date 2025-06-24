a07a5d2dbd3fd1b257776bbeb9c854c4696fa40b
Ari Widi <ari@example.com>
2025-06-24 23:18:49 +0700

    update serviceProviders

--- database/migrations/2025_06_17_072250_create_data_donasi_table.php
+++ database/migrations/2025_06_17_072250_create_data_donasi_table.php
@@ -14,7 +14,7 @@ class CreateDataDonasiTable extends Migration
             $table->text('nama_donasi');
             $table->text('kategori_donasi');
             $table->text('target_donasi');
-            $table->bigint('perolehan_donasi');
+            $table->bigInteger('perolehan_donasi');
             $table->date('masa_donasi');
             $table->text('deskripsi_donasi');
             $table->date('tgl_donasi');
